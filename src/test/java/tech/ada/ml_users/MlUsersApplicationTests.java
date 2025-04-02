1c2ca84ee70e59ad1aebc53ada8e3d286d023c47
rflima11 <rodolfo.lima.ferreira11@gmail.com>
2025-04-02 15:39:12 -0300

    primeira aula

---
+++ src/test/java/tech/ada/ml_users/MlUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ada.ml_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

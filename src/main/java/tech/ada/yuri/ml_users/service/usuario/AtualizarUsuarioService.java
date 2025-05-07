490acd3213ba9032f635283c63e5c8746feaa1f5
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-05-07 17:07:10 -0300

    como atualizar usuario está a parte de criar, apenas mexendo em usuario, adicionei a logica de bcrypt independente em atualizar usuario também

--- src/main/java/tech/ada/yuri/ml_users/service/usuario/AtualizarUsuarioService.java
+++ src/main/java/tech/ada/yuri/ml_users/service/usuario/AtualizarUsuarioService.java
@@ -1,5 +1,6 @@
 package tech.ada.yuri.ml_users.service.usuario;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import tech.ada.yuri.ml_users.dto.usuario.AtualizarUsuarioRequestDTO;
@@ -11,15 +12,18 @@ import tech.ada.yuri.ml_users.repository.UsuarioRepository;
 public class AtualizarUsuarioService {
     private final BuscarUsuarioService buscarUsuarioService;
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AtualizarUsuarioService(BuscarUsuarioService buscarUsuarioService, UsuarioRepository usuarioRepository) {
+    public AtualizarUsuarioService(BuscarUsuarioService buscarUsuarioService, UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
         this.buscarUsuarioService = buscarUsuarioService;
         this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Usuario atualizarUsuario(AtualizarUsuarioRequestDTO usuarioAtualizado, Long id) {
         Usuario usuarioExistente = buscarUsuarioService.buscarUsuarioPorId(id);
         AtualizarUsuarioRequestMapper.updateEntity(usuarioExistente, usuarioAtualizado);
+        usuarioExistente.setSenha(passwordEncoder.encode(usuarioExistente.getSenha()));
         return usuarioRepository.save(usuarioExistente);
     }
 

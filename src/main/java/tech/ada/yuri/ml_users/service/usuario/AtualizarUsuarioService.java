0fab41782ce9215ad056ea4778aeb85476dd3bb3
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-05-07 16:16:02 -0300

    altera atualizar usuario service para usar usuario repository e só alterar usuario sem mexer no endereço

--- src/main/java/tech/ada/yuri/ml_users/service/usuario/AtualizarUsuarioService.java
+++ src/main/java/tech/ada/yuri/ml_users/service/usuario/AtualizarUsuarioService.java
@@ -1,24 +1,26 @@
 package tech.ada.yuri.ml_users.service.usuario;
 
 import org.springframework.stereotype.Service;
+
 import tech.ada.yuri.ml_users.dto.usuario.AtualizarUsuarioRequestDTO;
 import tech.ada.yuri.ml_users.dto.usuario.mapper.AtualizarUsuarioRequestMapper;
 import tech.ada.yuri.ml_users.model.Usuario;
+import tech.ada.yuri.ml_users.repository.UsuarioRepository;
 
 @Service
 public class AtualizarUsuarioService {
     private final BuscarUsuarioService buscarUsuarioService;
-    private final CriarUsuarioService criarUsuarioService;
+    private final UsuarioRepository usuarioRepository;
 
-    public AtualizarUsuarioService(BuscarUsuarioService buscarUsuarioService, CriarUsuarioService criarUsuarioService) {
+    public AtualizarUsuarioService(BuscarUsuarioService buscarUsuarioService, UsuarioRepository usuarioRepository) {
         this.buscarUsuarioService = buscarUsuarioService;
-        this.criarUsuarioService = criarUsuarioService;
+        this.usuarioRepository = usuarioRepository;
     }
 
     public Usuario atualizarUsuario(AtualizarUsuarioRequestDTO usuarioAtualizado, Long id) {
         Usuario usuarioExistente = buscarUsuarioService.buscarUsuarioPorId(id);
         AtualizarUsuarioRequestMapper.updateEntity(usuarioExistente, usuarioAtualizado);
-        return criarUsuarioService.criarUsuario(usuarioExistente);
+        return usuarioRepository.save(usuarioExistente);
     }
 
 }
